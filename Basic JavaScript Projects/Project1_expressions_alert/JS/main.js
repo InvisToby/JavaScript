b035d3cc029bfe830429539f53e367e41d392773
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-02-24 11:58:14 +0000

    More Assignments

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,22 @@
 window.alert("Still, Hello Worlds!");
 
-var A ="Var, Hello Worlds";
+var A = "Var, Hello Worlds. this is a string";
 
 document.write(A);
+//Assignment 7
+var B = "Many attempts later.";
+
+window.alert(B);
+//Assignment 8
+var C = "<br>just a copy.<br> Lisa Told Bart\"Knock it off, Bart!\"<br>";
+
+document.write(C);
+
+document.write("\"Eat my Shorts!\" Bart Responded.<br> Copy ended");
+//Assignment 9 Concatenate 
+document.write();
+
+//challenge 1
+
+//Assignment 10 mulitple variables
+var Family="Atomic", Dad="", Mum="" 
