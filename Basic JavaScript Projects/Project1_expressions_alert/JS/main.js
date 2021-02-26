ed342108f044966132b37d78554ae13da9931e2f
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-02-26 12:10:42 +0000

    complete cassignment 9

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -12,11 +12,26 @@ var C = "<br>just a copy.<br> Lisa Told Bart\"Knock it off, Bart!\"<br>";
 
 document.write(C);
 
-document.write("\"Eat my Shorts!\" Bart Responded.<br> Copy ended");
+document.write("\"Eat my Shorts!\" Bart Responded.<br> Copy ended<br>");
 //Assignment 9 Concatenate 
-document.write();
+document.write("trying to concentrate."
++ " But can\'t."
++ " But trying anyways.");
 
 //challenge 1
 
 //Assignment 10 mulitple variables
-var Family="Atomic", Dad="", Mum="" 
+var Family="Atomic", Dad="Alex", Mum="Redacted", Son="Toby", Cousin="Orson";
+//Assignment 11
+var Son=Son.fontcolor("blue");
+var Family=family.fontcolor("yellow");
+var Dad=Dad.fontcolor("purple"); 
+var Mum=Mum.fontcolor("navy"); 
+var Cousin=Cousin.fontcolor("maroon");
+
+document.write(Son);
+
+document.write(2+8);
+
+
+
