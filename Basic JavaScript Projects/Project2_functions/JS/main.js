848bec03092632bfab956610accd60cbd95b58e4
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-03-01 12:01:32 +0000

    Assignment 13 functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+//assignment 13
+function My_First_Function(){ //defining/create function by naming it
+    var str = "A test of sorts"; //defining/create variable by giving it a string value
+    var result =  str.fontcolor("blue"); //using the fontcolor method on var string 'str' 
+    document.getElementById("aTest").innerHTML=result; //putting value of result into HTML element with 'aTest'
+}
+
+//assignment 13
