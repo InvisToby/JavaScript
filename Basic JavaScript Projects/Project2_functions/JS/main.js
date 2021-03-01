e4ce83c4cbc6f0d9ec00bce8cac1d06958170d9d
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-03-01 12:10:03 +0000

    assignment 14 function concatenate

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -5,4 +5,9 @@ function My_First_Function(){ //defining/create function by naming it
     document.getElementById("aTest").innerHTML=result; //putting value of result into HTML element with 'aTest'
 }
 
-//assignment 13
+//assignment 14
+function concatenate(){ //defining/create function by naming it 'concatenate'
+    var sentence = "A concatenate test of sorts"; //defining/create variable by giving it a string value
+    sentence += " to combine these two sentences"; //using the fontcolor method on var string 'str' 
+    document.getElementById("concatenated").innerHTML=sentence; //putting value of result into HTML element with 'aTest'
+}
