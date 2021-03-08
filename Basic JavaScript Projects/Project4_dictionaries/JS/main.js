49f42b58bed2ef25420b7fcd00b6b9ca3ba6a0ea
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-03-08 10:29:19 +0000

    added a delete statement that removes the key before its value is displayed

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,4 +1,20 @@
+//assignment 25
 function original_Character() {
+    var Original = {
+        Name:"Toby",
+        DOB: "10/10/1997",
+        Age: "23",
+        Pronouns: "He They Toby",
+        Height: "6ft",
+        Eye_Colour: "Light Blue",
+        Quote:"Please take care of yourselves,
+    };
+    document.getElementById("Characters").innerHTML = Original.Quote;
+}
+//Orginal character KVP for ''Toby showing 'quote on main Html page
+
+//assignment 25
+function del_Original_Character() {
     var Original = {
         Name:"Toby",
         DOB: "10/10/1997",
@@ -9,5 +25,7 @@ function original_Character() {
         Quote:"Please take care of yourselves",
         Quote:"What are you doing" //challenge 5 writes out this quote isntead of the one before
     };
-    document.getElementById("Characters").innerHTML = Original.Quote;
+    delete Original.Quote;
+    document.getElementById("delCharacters").innerHTML = Original.Quote;
 }
+//will show undefined on main Html page for 'quote' as the value is no longer there
