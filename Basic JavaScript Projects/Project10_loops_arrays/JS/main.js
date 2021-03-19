40fb1e3cf8c72d596276e3e2997113fd1d7527b0
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-03-19 10:21:35 +0000

    loops

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,13 @@
+//assignment 50
+
+function count_To_Ten() {
+    var Digit = "";
+    var x = 1;
+    while (x < 11) {
+        digit += "<br>" + x;
+        x++;
+    }
+    document.getElementById("Counting_to_Ten").innerHTML = Digit;
+}
+
+//challenge 13
