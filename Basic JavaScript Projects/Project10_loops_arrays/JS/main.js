b2e886e1603a969014e191538fa9e3064f7876b7
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-03-23 12:59:01 +0000

    length and a for Loop

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,16 +1,33 @@
 //assignment 50
-
-function count_To_Ten() {
+//while Loop
+function Call_Loop() {
     var Digit = "";
-    var x = 1;
-    while (x < 11) {
-        digit += "<br>" + x;
-        x++;
+    var X = 1;
+    while (X < 11) { //while x is less then 11 perform continue loop
+        Digit += "<br>" + X; //digit =+ x on a new line 
+        X++; //add 1
     }
-    document.getElementById("Counting_to_Ten").innerHTML = Digit;
+    document.getElementById("Loop").innerHTML = Digit;
 }
 
 //challenge 13
 //The length property returns the length of a string (number of characters).
 
+function find_length(){
+    var str = "find";
+    var n = str.length;
+    document.getElementById("found").innerHTML = n;
+}
+
 //assignment 51
+//for Loop
+
+    var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+    var Content = "";
+    var Y;
+    function For_Loop(){
+        for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";    
+        }
+        document.getElementById("List_of_Instruments").innerHTML = Content;
+}
