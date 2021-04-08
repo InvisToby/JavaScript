702852612151a19fd478474a232ece48862d5450
InvisToby <71771123+InvisToby@users.noreply.github.com>
2021-04-08 11:21:13 +0100

    Commenting code

--- Basic JavaScript Projects/Project10_loops_arrays/JS/challenge.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/challenge.js
@@ -1,10 +1,12 @@
+//submission 10
+
 //challenge 14
 //return statement.
 //stops the execution of a function and returns a value from that function
 
 var A = mathFunction(56, 5); // Function called, return value ends up in A
 function mathFunction(a, b) {
-  return a + b;   // returns the product of a and b
+  return a + b;   // returns the product of a + b
 }  
 document.getElementById("mathReturn").innerHTML = A;
 
@@ -18,16 +20,18 @@ document.getElementById("Hello").innerHTML = helloFunction("Moderator");
 var text = "";
 var i;
 for (i = 1; i < 10; i++) {
-  if (i === 5) { break; }
+  if (i === 5) { break; } //if i = 5, will 'jump out' of loop obly going up to 4, even tho it can go up to 10
   text += "You will be player " + i + "<br>";
 }
 document.getElementById("break_Test").innerHTML = text;
+//should print "You will be player 1"  up to until "You will be player 4"
 
 //The continue statement "jumps over", skips, one iteration in the loop.
 var text = "";
 var i;
 for (i = 0; i < 10; i++) {
-  if (i === 4) { continue; }
+  if (i === 4) { continue; } //if i = 4 skips jumps over 4
   text += "The number is " + i + "<br>";
 }
 document.getElementById("continue_Test").innerHTML = text;
+//should display 'The number is 0 -3' then skips The 'number is 4 before' continuing 'The number is 5'
--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,10 +1,13 @@
+//submission 10
+
 //assignment 50
 //while Loop
+//will execute a block of code as long as a specified condition is true 
 function Call_Loop() {
     var Digit = "";
     var X = 1;
     while (X < 11) { //while x is less then 11 perform continue loop
-        Digit += "<br>" + X; //digit =+ x on a new line 
+        Digit += "<br>" + X; //digit will = x on a new line becase of = <br> 
         X++; //add 1
     }
     document.getElementById("Loop").innerHTML = Digit;
@@ -21,13 +24,14 @@ function find_length(){
 
 //assignment 51
 //for Loop
-
-var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
-var Content = "";
-var Y;
-function For_Loop(){
-    for (Y = 0; Y < Instruments.length; Y++) {
-    Content += Instruments[Y] + "<br>";    
+//for when you know exactly how many times you want to loop through a block of code
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"]; //sets a variable Instruments, and an array?
+var Content = ""; //sets a variable
+var Y; //sets a variable
+function For_Loop(){ //will loop till all are done
+    for (Y = 0; Y < Instruments.length; Y++) { //1st statement start var init Y = 0, 2nd statement (condition for the loop to run) if Y lenth is less the Insruments array, 3rs statement, increases value (++) each time the code in the loop has been executed
+    //Id returns true, the loop will start over again, if it returns false, the loop will end.
+    Content += Instruments[Y] + "<br>"; //Content = Instruments what ever [Y] is at time, repeats   
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
@@ -36,45 +40,53 @@ function For_Loop(){
 //Array
 
 function array_Function(){
-    var Oc_Profile = [];
-    Oc_Profile[0] = "red haired";
-    Oc_Profile[1] = "blue eyes";
+    var Oc_Profile = []; // Oc_Profile is the Object
+    Oc_Profile[0] = "red haired"; //[0] = indexes
+    Oc_Profile[1] = "blue eyes"; //'blue eyes' = properties of the object
     Oc_Profile[2] = "an artist";
     Oc_Profile[3] = "hard working";
-    document.getElementById("Array").innerHTML = "They are " + Oc_Profile[3] + ".";
+    document.getElementById("Array").innerHTML = "They are " + Oc_Profile[3] + "."; //gets ocprofile[3] will display They are Hard working.
 }
 
 //assignment 53
 //const
-
-function constant_function(){
+//cannot be reassigned It does NOT define a constant value. It defines a constant reference to a value.
+function constant_function(){ 
     const Profile = {name:"Cipher", race:"Alien", hair:"Red Hair", strength:"Super resistant to practically everything"};
-    Profile.hair = "red haired";
-    Profile.eyes = "Blue eyes";
-    document.getElementById("Constant").innerHTML = "" + Profile.name + " has " + Profile.eyes + ". and is " + Profile.strength;
+    //the const 'Profile' cannot be reassigned
+    Profile.hair = "Blue hair"; //can change the value
+    Profile.eyes = "Blue eyes"; //can add a value
+    document.getElementById("Constant").innerHTML = "" + Profile.name + " has " + Profile.eyes + " and " + Profile.hair + ". and is " + Profile.strength;
 }
 
 //assignment 54
 //let
 var L = 22;
+//L=22
 document.write(L);
 {
     let L = 87;
     document.write ("<br>" + L)
+    //L = 87
 }
+// L can NOT be used here
+//L=22
 document.write ("<br>" + L)
 
+
 //assignment 55
-let car = {
-    make: "Dodge ",
+//objects
+let car = { //object 
+    make: "Dodge ", //properties = 'makel, 'model'
     model: "Viper ",
     year: "2021 ",
     color: "red ",
-    description : function() {
+    description : function() { //is the method
         return "this car is a " + this.year + this.color + this.make + this.model;
     }
 };
 document.getElementById("car_object").innerHTML = car.description();
+//method = this car is a 2021 red dodge viper.
 
 //assignment 55a
 let ocProfile = {
